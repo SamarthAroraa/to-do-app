386e8180dc024df1fbd79049d40994368dbdc35d
Samarth Arora <samartharora@Samarths-MacBook-Air.local>
2020-05-19 21:20:22 +0530

    deletion bug fix 3.0

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,12 +3,13 @@ var check = $(".check");
 //this function gets parent of checkbox, then applies strike thorugh, then applies fade in, the redirects to deletion link for that id
 check.click(function () {
   let id = "#" + $(this).attr("value");
+  let delid = $(this).attr("value");
   console.log($(id));
   $(id).parent().css("text-decoration", "line-through");
   $(id)
     .parent()
     .fadeOut(1000, function () {
-      let url = window.location.href + "strike-off/" + $(this).attr("value");
+      let url = window.location.href + "strike-off/" + delid;
       window.location.replace(url);
     });
 });
