d849c3754d3222dd6449926c3d5f1fdfac4e2eaf
Samarth Arora <samartharora@Samarths-MacBook-Air.local>
2020-05-19 17:06:06 +0530

    single task deletion bug fixes. class selector as causing the first element in stack to be deleted. replaced with %(this).

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,12 @@
-var check = $("#check");
+var check = $(".check");
 
 //this function gets parent of checkbox, then applies strike thorugh, then applies fade in, the redirects to deletion link for that id
 check.click(function () {
-  console.log($("#check").parents());
-  $("#check").parents().eq(2).css("text-decoration", "line-through");
-  jQuery("#check")
-    .parents()
-    .eq(3)
+  let id = "#" + $(this).attr("value");
+  console.log($(id));
+  $(id).parent().css("text-decoration", "line-through");
+  $(id)
+    .parent()
     .fadeOut(1000, function () {
       let url = window.location.href + "strike-off/" + check.attr("value");
       window.location.replace(url);
