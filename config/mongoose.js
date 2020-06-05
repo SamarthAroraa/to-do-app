9a9b7bee6d00939e98bcbae7ae4679fa2a22213a
Samarth Arora <samartharora@Samarths-MacBook-Air.local>
2020-06-05 05:56:13 +0530

    heroku adjustments

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/todo_development" || p
+  process.env.MONGODB_URI || "mongodb://localhost/todo_development"
 );
 
 const db = mongoose.connection;
